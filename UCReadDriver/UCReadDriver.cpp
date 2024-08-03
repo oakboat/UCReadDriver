4df31d252a18a403a0821ee675ccb85d275f549c
oakboat <27909321+oakboat@users.noreply.github.com>
2024-08-03 11:03:39 +0800

    add GetProcessIDByName

--- UCReadDriver/UCReadDriver.cpp
+++ UCReadDriver/UCReadDriver.cpp
@@ -2,6 +2,43 @@
 #include "driver_data.h"
 #include "driver_loader.h"
 #include "driver.h"
+#include <TlHelp32.h>
+
+DWORD GetProcessIDByName(const std::wstring& processName) {
+    HANDLE hProcessSnap;
+    PROCESSENTRY32W pe32;
+    DWORD processID = 0;
+
+    // Take a snapshot of all processes in the system
+    hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hProcessSnap == INVALID_HANDLE_VALUE) {
+        std::wcerr << L"CreateToolhelp32Snapshot failed!" << std::endl;
+        return 0;
+    }
+
+    // Fill in the size of the structure before using it
+    pe32.dwSize = sizeof(PROCESSENTRY32W);
+
+    // Retrieve information about the first process,
+    // and exit if unsuccessful
+    if (!Process32FirstW(hProcessSnap, &pe32)) {
+        std::wcerr << L"Process32FirstW failed!" << std::endl;
+        CloseHandle(hProcessSnap);          // Clean the snapshot object
+        return 0;
+    }
+
+    // Now walk the snapshot of processes
+    do {
+        if (_wcsicmp(pe32.szExeFile, processName.c_str()) == 0) {
+            processID = pe32.th32ProcessID;
+            break;
+        }
+    } while (Process32NextW(hProcessSnap, &pe32));
+
+    CloseHandle(hProcessSnap);
+    return processID;
+}
+
 
 int main()
 {
@@ -35,11 +72,15 @@ int main()
     std::cout << "Base Address: " << my_driver.get_base_section_address(pid) << "\n";
     std::cout << "Read value: " << my_driver.read_process_memory<int>(pid, (ULONG64) & test_value) << "\n";
 
+    pid = GetProcessIDByName(L"notepad.exe");
+    std::cout << "Base Address: " << my_driver.get_base_section_address(pid) << "\n";
+    std::cout << "Read value: " << my_driver.read_process_memory<int>(pid, my_driver.get_base_section_address(pid)) << "\n";
+
     uintptr_t thread_id = 0;
     my_driver.get_dwm_thread_id(pid, &thread_id);
     while (thread_id == 0 && !(GetAsyncKeyState(VK_F1) & 0x8000))
     {
-        Sleep(1000);
+        Sleep(100);
     }
     std::cout << thread_id << "\n";
     my_driver.get_dwm_thread_id(0, 0);
